92ad8701d61b0c82185c34d54af54bdd211ac3a4
Ben Cooper <ben.cooper.226@gmail.com>
2021-01-27 20:04:26 +0000

    Initial commit

---
+++ ultron_tabulate.R
@@ -0,0 +1,53 @@
+# Packages
+require(tidyverse)
+require(readxl)
+require(foreach)
+
+# Load
+file <- "P:/LIDA/OneDrive - University of Leeds/Ultron/Guide.xlsx"
+films <- read_xlsx(file, 1)
+pool <- read_xlsx(file, 2)
+vars <- read_xlsx(file, 3)
+
+# Produce full list
+films_unique <- unique(films$Film)
+full_rules <- foreach(i = 1:length(films_unique),
+        .combine = "rbind") %do%
+  {
+    # Extract title-only rules
+    title <- films_unique[i]
+    title_only <- films %>% 
+      filter(Film == title) %>% 
+      select(2:3)
+    
+    # Extract applicable variables
+    valid_vars <- vars %>% 
+      filter(Film == title) %>% 
+      pivot_longer(2:ncol(vars)) %>% 
+      filter(value == "Y")
+    
+    # Extract applicable rules
+    valid_rules <- pool %>% 
+      filter(Requires %in% valid_vars$name) %>% 
+      select(c(1,3))
+    
+    # Prepare output
+    tibble(Film = title,
+           rbind(title_only,
+                 valid_rules))
+    
+  }
+
+# Tabulate
+tabulated <- full_rules %>% 
+  na.omit() %>% 
+  group_by(Film, Class) %>% 
+  summarise(n = n()) %>% 
+  pivot_wider(names_from = "Class",
+              values_from = "n")
+
+# Write
+write_csv(tabulate, "UltronTabulated.csv")
+full_rules %>% 
+  na.omit() %>% 
+  write_csv("UltronRules.csv")
