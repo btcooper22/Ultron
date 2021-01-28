5024695b94e7e06daf0504520b2d83363c557aac
Ben Cooper <ben.cooper.226@gmail.com>
2021-01-28 10:39:58 +0000

    First fully working version

--- Ultron/app.R
+++ Ultron/app.R
@@ -1,11 +1,11 @@
-# FUll randomisation
-# Control film options
-# Control number within each category
-# Render images
+# To-do: Render images
+# To-do: Better IO
+
 
 # Packages
 require(shiny)
 require(dplyr)
+require(tibble)
 require(shinyWidgets)
 
 # Data (for deployment)
@@ -16,50 +16,122 @@ rules <- read.csv("UltronRules.csv")
 
 # Define UI for application
 ui <- fluidPage(
-
+    
     # Application title
     titlePanel("Ultron: A Marvel Drinking Game Generator"),
-
+    
     sidebarLayout(
         sidebarPanel(
             helpText("Select films to include or exclude in the randomisation:"),
             
             pickerInput("film", "Films:",
                         choices = unique(rules$Film),
-                        multiple = TRUE, width = "auto", 
-                        options = list(`actions-box` = TRUE)),
+                        multiple = TRUE, width = "fit", 
+                        options = list(`actions-box` = TRUE,
+                                       `selected-text-format` = "count > 1"),
+                        selected = unique(rules$Film)),
             
-            sliderInput("range", 
-                        label = "Range of interest:",
-                        min = 0, max = 100, value = c(0, 100)),
-            actionButton("goButton","Randomize!")
+            numericInput("nS", "Small drinks:", min = 0, max = 12, value = 2),
+            numericInput("nM", "Medium drinks:", min = 0, max = 8, value = 2),
+            numericInput("nL", "Large drinks:", min = 0, max = 6, value = 2),
+            actionBttn("goButton","Randomize!",
+                       style = "gradient")
         ),
         
         mainPanel(
-            textOutput("selected_var"),
-            textOutput("min_max")
+            textOutput("selected_film"),
+            htmlOutput("small_drink"),
+            htmlOutput("medium_drink"),
+            htmlOutput("large_drink")
         )
     )
 )
 
 # Debug
-#input <- data.frame(film = unique(rules$Film))
+# input <- data.frame(film = unique(rules$Film),
+#                     nS = 2)
+# film <-  sample(input$film,1)
+# film_rules <- rules %>% 
+#     filter(Film == film)
+
+paste("Take a small drink whenever", c("A", "B", "C"))
 
 # Define server logic 
 server <- function(input, output) {
-    # Generate film
     film <- eventReactive(input$goButton, {
-        sample(input$film,1)
+        # Generate film
+        film_name <- sample(input$film,1)
+        
+        # Isolate rules
+        film_rules <- rules %>% 
+            filter(Film == film_name)
+        
+        # Sample small drinks
+        small_drinks <- film_rules %>% 
+            filter(Class == "S") %>% 
+            slice_sample(n = input$nS) %>% 
+            select(Rule) %>% 
+            deframe()
+        
+        # Sample medium drinks
+        medium_drinks <- film_rules %>% 
+            filter(Class == "M") %>% 
+            slice_sample(n = input$nM) %>% 
+            select(Rule) %>% 
+            deframe()
+        
+        
+        # Sample large drinks
+        large_drinks <- film_rules %>% 
+            filter(Class == "L") %>% 
+            slice_sample(n = input$nL) %>% 
+            select(Rule) %>% 
+            deframe()
+        
+        # Return
+        list(film_name = film_name,
+             small_drinks = small_drinks,
+             medium_drinks = medium_drinks,
+             large_drinks = large_drinks)
     })
     
     
-    output$selected_var <- renderText({ 
-        paste("Your film is", film())
+    output$selected_film <- renderText({ 
+        paste("Your film is", film()["film_name"])
     })
     
-    output$min_max <- renderText({ 
-        paste("You have chosen a range that goes from",
-              input$range[1], "to", input$range[2])
+    output$small_drink <- renderUI({ 
+        if(input$nS!=0)
+        {
+            HTML(paste("Take a small drink whenever", film()["small_drinks"] %>% unlist(), 
+                       collapse  = "<br/>"))
+        }else
+        {
+            HTML("")
+        }
+        
+    })
+    
+    output$medium_drink <- renderUI({ 
+        if(input$nM!=0)
+        {
+            HTML(paste("Take a medium drink whenever", film()["medium_drinks"] %>% unlist(), 
+                       collapse  = "<br/>"))
+        }else
+        {
+            HTML("")
+        }
+    })
+    
+    output$large_drink <- renderUI({ 
+        if(input$nL!=0)
+        {
+            HTML(paste("Take a large drink whenever", film()["large_drinks"] %>% unlist(), 
+                       collapse  = "<br/>"))
+        }else
+        {
+            HTML("")
+        }
     })
 }
 
